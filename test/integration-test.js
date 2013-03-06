26b9f4299d3bbb02331d00da505f3ce46df18219
Thomas Peklak <thomas.peklak@gmail.com>
2013-03-06 21:51:06 +0100

    check for existence of file

--- test/integration-test.js
+++ test/integration-test.js
@@ -1,27 +1,32 @@
 "use strict";
+/*global describe:false, it:false */
 
 var request = require("supertest");
 var Resizer = require("../");
 var Preset = require("../preset");
+var express = require("express");
+var fs = require("fs");
+
+var cleanup = function (path, cb) {
+    return function () {
+        fs.unlink(path, cb);
+    };
+};
 
 describe("resizer", function () {
-    it("should resize images on the fly", function (done) {
-        var express = require("express");
+    it("should return images", function (done) {
         var app = express();
 
         app.use(express.static(__dirname + "/test-images"));
         var resizer = new Resizer();
         resizer.attach((new Preset("name"))
-                       .publicDir(__dirname)
-                       .from("/test-images")
-                       .resize({
-                           width: 100,
-                           height: 100
-                       })
-                       .to("/test-out-images"));
+            .publicDir(__dirname)
+            .from("/test-images")
+            .to("/test-out-images"));
         app.use(resizer.app);
         request(app)
             .get("/test-out-images/profile.png")
-            .expect(200, done);
+            .expect(200, cleanup(__dirname + "/test-out-images/profile.png", done));
     });
+
 });
